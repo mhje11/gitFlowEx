95ab10e19ce598594537b84216f8fa35d94c06cd
LIM HEE JIN <xeexin0331@gmail.com>
2024-06-17 16:40:43 +0900

    heejin

--- src/PrintAnything.java
+++ src/PrintAnything.java
@@ -1,5 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class PrintAnything {
     public static void main(String[] args) {
         System.out.println("임영규");
+        System.out.println("임희진");
+
     }
+
 }
